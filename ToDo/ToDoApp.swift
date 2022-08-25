b30adfded8c1d1caba7cbef538c9d9df1a74c19d
vetka4ever <dansh@sfedu.ru>
2022-08-25 10:57:33 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Daniil on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
